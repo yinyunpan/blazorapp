28dad93a89593860f01320edc2cbf673e121efd0
殷允攀 <yyp12556@ly.com>
2022-10-16 13:59:46 +0800

    不用删除

--- src/BlazorAppElectron/Program.cs
+++ src/BlazorAppElectron/Program.cs
@@ -1,4 +1,3 @@
-using BlazorAppElectron.Data;
 using ElectronNET.API;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddAntDesign();
 
 builder.WebHost.UseElectron(args);
